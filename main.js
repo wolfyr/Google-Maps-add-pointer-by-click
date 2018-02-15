dec3f83406226c7bc3ceba628b31d878c58cce2f
wolfyr <35544104+wolfyr@users.noreply.github.com>
2018-02-15 20:14:41 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function initMap(){
+  var options = {
+    zoom: 9,
+    center: {lat: 50.99833, lng: 5.86944}
+  }
+
+var map = new google.maps.Map(document.getElementById('map'), options);
+
+google.maps.event.addListener(map, 'click', function( event ){
+  alert("De latitude coordinaten zijn: " + event.latLng.lat() + "\nEn de Longtude coordiaten zijn:" + event.latLng.lng());
+  addMarker({lat: event.latLng.lat(), lng: event.latLng.lng()});
+});
+
+function addMarker(coords){
+  var marker = new google.maps.Marker({
+    position: coords,
+    map:map
+  });
+
+}
+
+
+
+}
